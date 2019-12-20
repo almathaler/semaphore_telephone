0a625f276a9b63170a8ccd6f52de8319912b6b32
almathaler <athaler00@stuy.edu>
2019-12-20 11:09:40 -0500

    touched files

---
+++ write.h
@@ -0,0 +1,10 @@
+/*
+A main writing program
+    Will attempt to access the resources using the semaphore
+        Once in, it should display the last line added to the file (use shared memory to
+        contain the size of the last line).
+        Prompt the use for the next line.
+        Once a new line is read, write that to the file, update the shared memory and then
+        release the semaphore
+You do not need a run target in your makefile
+*/
